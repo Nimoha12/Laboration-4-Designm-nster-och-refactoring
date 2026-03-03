2f1dc5bda859c2b1c5154c6a39d96dbe37948308
Nimoha12 <nimoa.haji@gmail.com>
2026-03-03 13:51:05 +0100

    Update VolvoWorkshop.java

--- VolvoWorkshop.java
+++ VolvoWorkshop.java
@@ -2,6 +2,6 @@ import java.awt.*;
 
 public class VolvoWorkshop extends Workshop<Volvo240> {
     public VolvoWorkshop(int capacity, Point position){
-        super(capacity,position, "pics/VolvoBrand.jpg");
+        super(capacity,position, 101, 96);
     }
 }
