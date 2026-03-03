f219dcf81f651f668b72c66ad2fcd985d7a714b0
Nimoha12 <nimoa.haji@gmail.com>
2026-03-03 13:44:51 +0100

    Refactor DrawPanel to use CarController and load images
    Refactor DrawPanel to use CarController for car and workshop management. Added image loading for cars and workshops.

--- DrawPanel.java
+++ DrawPanel.java
@@ -1,38 +1,64 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 // This panel represents the animated part of the view with the car images.
+@SuppressWarnings("ALL")
+public class DrawPanel extends JPanel {
+    private CarController controller;
 
-public class DrawPanel extends JPanel{
-    //list of cars from CarController
-    ArrayList<Car> cars;
-    ArrayList<Workshop<Volvo240>> workshops;
+    //Map that matches cars to respective car image
+    Map<Class<? extends Movable>, BufferedImage> images = new HashMap<>();
+
+    //Map for workshop pictures
+    Map<Class<? extends Workshop<?>>, BufferedImage> WorkshopImages = new HashMap<>();
+
 
     // Initializes the panel and reads the images
-    public DrawPanel(int x, int y, ArrayList<Car> cars, ArrayList<Workshop<Volvo240>> workshops) {
-        this.cars = cars;
-        this.workshops = workshops;
+    public DrawPanel(int x, int y, CarController controller) {
+        this.controller = controller;
         this.setDoubleBuffered(true);
         this.setPreferredSize(new Dimension(x, y));
         this.setBackground(Color.lightGray);
-    }
 
-    // This method is called each time the panel updates/refreshes/repaints itself
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        for (Car car : cars){
-            int x = (int) car.getX();
-            int y = (int) car.getY();
-            g.drawImage(car.getImage(), x, y, null);
+        try {
+            //Add car images
+            images.put(Volvo240.class, ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Volvo240.jpg")));
+            images.put(Saab95.class, ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Saab95.jpg")));
+            images.put(Scania.class, ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Scania.jpg")));
+
+            WorkshopImages.put(VolvoWorkshop.class, ImageIO.read(DrawPanel.class.getResourceAsStream("pics/VolvoBrand.jpg")));
+
+        } catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+        // This method is called each time the panel updates/refreshes/repaints itself
+        @Override
+        protected void paintComponent(Graphics g){
+            super.paintComponent(g);
+
+            for (Car car : controller.getCars()) {
+                BufferedImage image = images.get(car.getClass());
+
+                int carX = (int) car.getX();
+                int carY = (int) car.getY();
+                if (image !=null) {
+                    g.drawImage(image, carX, carY, null);
+            }
         }
 
-        for (Workshop<?> ws : workshops){
-            BufferedImage img = ws.getImage();
-            Point pos = ws.getPosition();
-            if(img !=null && pos !=null){
-            g.drawImage(img, pos.x, pos.y, null);
-        }}
-    }}
+            for (Workshop<?> ws : controller.getWorkshops()) {
+                BufferedImage img = WorkshopImages.get(ws.getClass());
+                Point pos = ws.getPosition();
+                if (img != null && pos != null) {
+                    g.drawImage(img, pos.x, pos.y, null);
+                }
+            }
+        }
+    }
